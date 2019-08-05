bbc985c8a1e0b1d63d462e0ec5ccf6932d436aa5
Mohammad Asif Zaman <39745895+zaman13@users.noreply.github.com>
2019-08-04 18:51:40 -0700

    Update main.cpp

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -1,5 +1,5 @@
 // Mohammad Asif Zaman
-// July 27, 2019
+// July-August, 2019
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -11,78 +11,90 @@
 using namespace sf;
 
 
+
+// Define colors
 const Color dead_cell(26,27,29);
-// const Color live_cell(2,252,251);
-const Color live_cell(152,121,2);
-
-// const Color fish_color(2,252,251);
-
-const Color fish_color(152,121,2);
+const Color fish_color(2,252,251);
 const Color shark_color(252,81,81);
 
+// const Color fish_color(152,121,2);
+// const Color fish_color(190,190,20);
+
+
+// Defining window size
+const unsigned int window_size_x = 700;  // x size of the grid in pixels
+const unsigned int window_size_y = 400;  // y size of the grid in pixels
+const unsigned int window_extra_y = 220;  // Extra y space after the grid for text and plots
+
+
+// Define the size of each cell in pixels
 const unsigned int cell_size_x = 2;
 const unsigned int cell_size_y = 2;
-
-
-
 const unsigned int cell_border_thickness = 0;
 
+// Calculate number of cells based on the window size and cell size
+const unsigned int Nx = window_size_x/cell_size_x;
+const unsigned int Ny = window_size_y/cell_size_y;
 
-const unsigned int window_size_x = 800;
-const unsigned int window_size_y = 400;
-const unsigned int window_extra_y = 220;
 
-
+// Window offset and other parameters for display
 const unsigned int offset_x = 0;
 const unsigned int offset_y = 5;
 const unsigned int plot_offset_x = 50;
 const unsigned int plot_offset_y = window_extra_y -20;
 
 
-const unsigned int Nx = window_size_x/cell_size_x;
-const unsigned int Ny = window_size_y/cell_size_y;
+// Predetor-prey dynimics parameters
+const unsigned int fbreed = 3;   // fish breed time
+const unsigned int sbreed = 6;   // shark breed time
+const unsigned int starve_max = 4;  // shark maximum starve time
 
-const unsigned int fbreed = 3;
-const unsigned int sbreed = 6;
-const unsigned int starve_max = 4;
+// Ratio for initial population
 const float fish_ratio = 0.01;
 const float shark_ratio = 0.005;
 
 
-
+// Arrays for storing fish/shark age and shark starve value
 unsigned int shark_population[Nx][Ny] = {0};
 unsigned int fish_population[Nx][Ny] = {0};
 unsigned int shark_starve[Nx][Ny] = {0};
 
 
-
+// Display update delay. Frame rate should be the inverse of this
 const float update_delay = 0.01;
 
 
 
 
+// Main function
+
 int main()
 {
 
-
-
-
-
-    // function prototype
-    int neighbor_counter(int arg1, int arg2, unsigned int arg3[Nx][Ny]);
+// function prototype
     void shark_move(int i, int j);
     void fish_move(int i, int j);
 
 
+// Random seed and start clock
     srand(time(0));
     Clock clk;
     clk.restart();    // starting a clock
 
+// Variables
+    bool wait = 1;                                        // Pause variable
+    unsigned int generation = 1;                          // counting generations
+    unsigned int n_generation_save = 500;                 // Number of generations to save and display in the plot
+    unsigned int counter_save = 0;                        // Counting how many generation data has been saved
+    unsigned int fish_count[n_generation_save] = {0};     // Array to store fish count at each generation
+    unsigned int shark_count[n_generation_save] = {0};    // Array to store shark count at each generation
 
-    // unsigned int random = rand() % 2;
 
+
+
+
+// Loading fonts
     Font font;
-
     if (!font.loadFromFile("Fonts/arial.ttf")){
         std::cout << "Error loading font from file" << std::endl;
         system("pause");
@@ -94,17 +106,17 @@ int main()
     std::string str = "Generation = " + oss.str();
 
 
-
+    // Defining texts
     Text text,text2,text3,text4;
     text.setFont(font);
     text.setString(str);
     text.setCharacterSize(12);
-    text.setPosition(window_size_x - 150,window_size_y + 10);
+    text.setPosition(window_size_x*.75, window_size_y + 10);
 
     text2.setFont(font);
     text2.setString("Press Space to Start/Pause\nPress Esc to quit");
     text2.setCharacterSize(12);
-    text2.setPosition(5, window_size_y + offset_y + 1);
+    text2.setPosition(15, window_size_y + offset_y + 1);
 
     text3.setFont(font);
     text3.setString("Time");
@@ -112,57 +124,66 @@ int main()
     text3.setPosition(window_size_x/2, window_size_y + window_extra_y -15);
 
     text4.setFont(font);
-    text4.setString("Population");
+    text4.setString("Fish Population,\nShark Population x 4");
     text4.setCharacterSize(10);
 
-    text4.setPosition(plot_offset_x-25, window_size_y + window_extra_y*.65);
+    text4.setPosition(plot_offset_x-35, window_size_y + window_extra_y*.7);
     text4.setRotation(-90.0);
 
+
+
+
+// Setting up drawing entities. VertexArrays and Rectangle shapes
+    VertexArray grid_draw(Quads,Nx*Ny*4);   // for drawing fish, shark and empty cells
+    VertexArray plot_axes(Lines,4);         // for drawing plot axes
+
+    RectangleShape cell(Vector2f(2,2));              // for drawing the plot
+    RectangleShape cell_legend_fish(Vector2f(5,5));  // for drawing the fish legend on the plot
+    RectangleShape cell_legend_shark(Vector2f(5,5)); // for drawing the shark legend on the plot
+
+    // cell.setFillColor(Color(2,252,251));
+
+// Initializing
+    cell.setFillColor(fish_color);
+    cell.setOutlineColor(Color(0,0,0));
+    cell.setOutlineThickness(cell_border_thickness);
+
+// Setting the position, color etc. of the plot legends
+    cell_legend_fish.setFillColor(fish_color);
+    cell_legend_fish.setOutlineColor(Color(0,0,0));
+    cell_legend_fish.setOutlineThickness(cell_border_thickness);
+    cell_legend_fish.setPosition(window_size_x*0.75-12,window_size_y + window_extra_y*.27);
+
+    cell_legend_shark.setFillColor(shark_color);
+    cell_legend_shark.setOutlineColor(Color(0,0,0));
+    cell_legend_shark.setOutlineThickness(cell_border_thickness);
+    cell_legend_shark.setPosition(window_size_x*0.75-12,window_size_y + window_extra_y*.27+13);
+
+    plot_axes[0].position = Vector2f(plot_offset_x,plot_offset_y + window_size_y);
+    plot_axes[1].position = Vector2f(window_size_x-plot_offset_x, plot_offset_y + window_size_y);
+    plot_axes[2].position = Vector2f(plot_offset_x,plot_offset_y + window_size_y);
+    plot_axes[3].position = Vector2f(plot_offset_x,  window_size_y + 50);
+
+
+
+// Render the window
     RenderWindow window(VideoMode(window_size_x,window_size_y + window_extra_y), "Predetor-Prey dynamics (Wa-Tor)");
     // window.clear(Color(216,27,29));
     window.clear(Color::Black);
 
 
 
-    VertexArray grid_draw(Quads,Nx*Ny*4);   // for drawinf fish, shark and empty cells
-    VertexArray plot_axes(Lines,4);
-
-    RectangleShape cell(Vector2f(2,2));
-
-    // cell.setFillColor(Color(2,252,251));
-    cell.setFillColor(live_cell);
-    cell.setOutlineColor(Color(0,0,0));
-    cell.setOutlineThickness(cell_border_thickness);
-
-
-    plot_axes[0].position = Vector2f(plot_offset_x,plot_offset_y + window_size_y);
-    plot_axes[1].position = Vector2f(window_size_x-plot_offset_x, plot_offset_y + window_size_y);
-
-    plot_axes[2].position = Vector2f(plot_offset_x,plot_offset_y + window_size_y);
-    plot_axes[3].position = Vector2f(plot_offset_x,  window_size_y + 50);
-
-    // plot_axes[4].position = Vector2f(window_size_x-plot_offset_x, plot_offset_y + window_size_y);
-    // plot_axes[5].position = Vector2f(window_size_x-plot_offset_x, window_size_y + 50);
-    //
-    // plot_axes[6].position = Vector2f(window_size_x-plot_offset_x, window_size_y + 50);
-    // plot_axes[7].position = Vector2f(plot_offset_x,  window_size_y + 50);
-    // plot_axes[2].position = Vector2f(window_size_x, plot_offset_y-300);
-    // plot_axes[2].position = Vector2f(plot_offset_x, plot_offset_y-300);
-
-
-
-
-
-
-
-
+// Initial populizing the grid
     for (int i = 0; i < Nx; i ++){
       for (int j = 0; j < Ny; j ++){
-        float temp = (rand() % 10000)/10000.0;
+        float temp = (rand() % 10000)/10000.0;  // Get a randome number between 0 and 1 with 4 decimal places
         // std:: cout << temp << std:: endl;
-        if (temp <= fish_ratio) fish_population[i][j] = rand() % fbreed + 1;  // randome interger between 1 and fbreed + 1
-        if (temp > fish_ratio && temp < fish_ratio + shark_ratio) shark_population[i][j] = rand() % sbreed + 1;  // randome interger between 1 and sbreed + 1
 
+        // populate grid based on the initial population ratio defined. Also, the cells are initialized with random age value between 1 and breeding age
+        if (temp <= fish_ratio) fish_population[i][j] = rand() % fbreed + 1;  // randome interger between 1 and fbreed
+        if (temp > fish_ratio && temp < fish_ratio + shark_ratio) shark_population[i][j] = rand() % sbreed + 1;  // randome interger between 1 and sbreed
+
+        // Check for overlap error
         if (fish_population[i][j] > 0 && shark_population[i][j] > 0) std:: cout << "Overlap error" << std:: endl;
 
       }
@@ -171,122 +192,100 @@ int main()
 
 
 
-    unsigned int generation = 1;
-    unsigned int n_generation_save = 500;
-    unsigned int counter_save = 0;
-
-
-    bool wait = 1;
-
-    unsigned int fish_count[n_generation_save] = {0};
-    unsigned int shark_count[n_generation_save] = {0};
 
 
     while(window.isOpen())
     {
 
+  // Variables
+      Event e;                            // Event variable
+      unsigned int x_array[Nx] = {0};     // Array with order index
+      unsigned int y_array[Ny] = {0};     // Array with order index
 
-      Event e;
-
-
-
-
-      while (window.pollEvent(e))
-      {
-        if (e.type == Event::Closed || e.key.code == Keyboard::Escape)
-          window.close();
-
-        if (e.type == Event::KeyPressed && e.key.code == Keyboard::Space)
-          wait = !wait;
-
-
-      }
-
-
-
-      // Update
-      // replace population_frame with next_frame
-
-      unsigned int x_array[Nx] = {0};
-      unsigned int y_array[Ny] = {0};
-
+      // Filling with sequential indices
       for (int i = 0; i < Nx; i ++) x_array[i] = i;
       for (int j = 0; j < Ny; j ++) y_array[j] = j;
 
+      // Randomizing order indices
       std::random_shuffle(&x_array[0], &x_array[Nx-1]);
       std::random_shuffle(&y_array[0], &y_array[Ny-1]);
 
 
 
+      // Check for keyboard presses
+      while (window.pollEvent(e))
+      {
+        if (e.type == Event::Closed || e.key.code == Keyboard::Escape)   window.close();
+        if (e.type == Event::KeyPressed && e.key.code == Keyboard::Space)  wait = !wait;
+      }
+
+
+
+// Update
+// Calculate the new location of the fishes and sharks
 
       if (clk.getElapsedTime().asSeconds() > update_delay && !wait)
       {
+        // Counters for counting fishes and sharks
         unsigned int fish_counter = 0;
         unsigned int shark_counter = 0;
 
-
+        // Nested loops to go through each grid point
         for (int ii = 0; ii < Nx; ii ++){
           for (int jj = 0; jj < Ny; jj ++){
             unsigned int i = x_array[ii];
             unsigned int j = y_array[jj];
 
-
+            // Check for conflicts (again)
             if (shark_population[i][j] > 0 && fish_population[i][j] > 0)  std:: cout << "Fish Shark conflict" << std:: endl;
 
+            // Call move functions only if the cell has a fish or a shark in it. Also count the fishes and sharks.
             if (shark_population[i][j] > 0)  {shark_move(i,j); shark_counter++;}
             if (fish_population[i][j] > 0)  {fish_move(i,j); fish_counter++;}
 
-
-
-
-            // live_cells = live_cells + next_frame[i][j];
           }
         }
 
+
+        // Save fish and shark counts for each generation
         fish_count[counter_save] = fish_counter;
         shark_count[counter_save] = shark_counter;
 
-        counter_save++;
-        if (counter_save >= n_generation_save){
-          counter_save = n_generation_save-1;
-          for (int k = 0; k < n_generation_save - 1; k++) {fish_count[k] = fish_count[k+1]; shark_count[k] = shark_count[k+1];}
+        counter_save++;   // increment save counter
 
+        // If counter_save is >= than the number of saved values we want, then left shift array by 1 (eliminating the first entry and making room at the last entry location)
+        if (counter_save >= n_generation_save){
+          counter_save = n_generation_save-1;   // Decrease counter save so that the last element is indexed
+          for (int k = 0; k < n_generation_save - 1; k++) {fish_count[k] = fish_count[k+1]; shark_count[k] = shark_count[k+1];}  // left shift the array
         }
 
-        generation = generation + 1;
+        generation = generation + 1;   // increment generation Number
+
+
+        // Display texts
         oss.str("");
         oss << generation;
         str = "Generation = " + oss.str();
-        //
+
         oss.str("");
         oss << fish_counter;
-        str = str + "\nNo. of fish = " + oss.str();
+        str = str + "\n\n\nFish population = " + oss.str();
 
         oss.str("");
         oss << shark_counter;
-        str = str + "\nNo. of shark = " + oss.str();
+        str = str + "\nShark population = " + oss.str();
 
-        //
-        //
-        // oss.str("");
-        // int temp = live_cells*10000/(Nx*Ny);
-        // float percent_live = temp/100.0;
-        // oss << percent_live;
-        // str = str + " (" + oss.str() + "%)";
-        //
-        //
-        //
-        //
+
         text.setString(str);
-        // text.setPosition(10,window_size_y + 5);
 
 
-        clk.restart();
+        clk.restart();   // Restart the clock (very important)
       }
 
 
-      unsigned int counter = 0;
+
       // Display
+      unsigned int counter = 0;
       for (int i = 0; i < Nx; i ++){
         for (int j = 0; j < Ny; j ++){
 
@@ -302,7 +301,7 @@ int main()
 
 
           if (fish_population[i][j] > 0){
-            cell.setFillColor(fish_color);
+            // cell.setFillColor(fish_color);
             grid_draw[counter].color = fish_color;
             grid_draw[counter+1].color = fish_color;
             grid_draw[counter+2].color = fish_color;
@@ -351,9 +350,6 @@ int main()
 
 
 
-
-
-
       window.display();
       window.clear(Color::Black);
 
@@ -361,10 +357,8 @@ int main()
       window.draw(text2);
       window.draw(text3);
       window.draw(text4);
-
-
-
-
+      window.draw(cell_legend_fish);
+      window.draw(cell_legend_shark);
 
 
     }  // window.isOpen
@@ -377,7 +371,110 @@ int main()
 
 
 
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
 // User defined functions
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void fish_move(int ix, int jy)
+{
+
+      // Arrays to save empty neigboring cell locations
+      int ix_free[8] = {0};
+      int jy_free[8] = {0};
+
+
+
+      int counter_free = 0;  // counter to count free neighboring cells
+
+      int x_ind = 0;
+      int y_ind = 0;
+
+      unsigned int old_pos_age = 0;
+      unsigned int new_pos_age = 0;
+
+
+      // aging + breeding
+      if (fish_population[ix][jy] >= fbreed){
+        old_pos_age = 1;
+        new_pos_age = 1;
+      } else{
+        old_pos_age = 0;
+        new_pos_age = fish_population[ix][jy] + 1;
+      }
+
+
+
+
+      // Nested loops to go through all the neighboring cells
+      for (int i = -1; i < 2; i++){
+        for (int j = -1; j < 2; j++ ){
+
+          // if condition to avoid counting its ownself
+          if (i*i + j*j != 0)
+          {
+
+            x_ind = ix + i;
+            y_ind = jy + j;
+
+
+            // Toroidal boundary condition
+            if (x_ind < 0) x_ind = Nx-1;
+            if (x_ind  >= Nx) x_ind = 0;
+            if (y_ind < 0) y_ind = Ny-1;
+            if (y_ind >= Ny) y_ind = 0;
+
+
+            // record position of empty cell
+            if (fish_population[x_ind][y_ind] == 0 && shark_population[x_ind][y_ind] == 0){
+              ix_free[counter_free] = x_ind;
+              jy_free[counter_free] = y_ind;
+              counter_free++;
+            }
+
+
+
+          } // end if i!=0 && j!=0
+
+
+        }  // end for j
+      } // end for i
+
+
+
+
+      // if the fish finds an empty neigboring cell
+      if (counter_free != 0){
+        // std:: cout << "empty cell movement" << std:: endl;
+        int temp1 = rand() % counter_free;
+
+        unsigned int iix = ix_free[temp1];
+        unsigned int jjy = jy_free[temp1];
+
+
+        fish_population[iix][jjy] = new_pos_age;  // move shark to new position with updated age
+        fish_population[ix][jy] = old_pos_age;   // update age at the previous position of the shark (0 for empty cell, 1 for new shark)
+      } else {  // fish does not find an empty neighboring cell
+
+        fish_population[ix][jy] = new_pos_age;
+
+
+      }
+
+
+
+
+}
+
 
 
 
@@ -527,109 +624,6 @@ void shark_move(int ix, int jy)
 
       // Note that if none of the above 2 if conditions are satisfied, the global veriables of the shark population and the fish population are not
       // updated. This is equivalent to the shark not moving.
-
-
-
-}
-
-
-
-
-
-
-
-
-
-void fish_move(int ix, int jy)
-{
-
-
-
-
-      // Arrays to save empty neigboring cell locations
-      int ix_free[8] = {0};
-      int jy_free[8] = {0};
-
-
-
-      int counter_free = 0;  // counter to count free neighboring cells
-
-      int x_ind = 0;
-      int y_ind = 0;
-
-      unsigned int old_pos_age = 0;
-      unsigned int new_pos_age = 0;
-
-
-      // aging + breeding
-      if (fish_population[ix][jy] >= fbreed){
-        old_pos_age = 1;
-        new_pos_age = 1;
-      } else{
-        old_pos_age = 0;
-        new_pos_age = fish_population[ix][jy] + 1;
-      }
-
-
-
-
-      // Nested loops to go through all the neighboring cells
-      for (int i = -1; i < 2; i++){
-        for (int j = -1; j < 2; j++ ){
-
-          // if condition to avoid counting its ownself
-          if (i*i + j*j != 0)
-          {
-
-            x_ind = ix + i;
-            y_ind = jy + j;
-
-
-            // Toroidal boundary condition
-            if (x_ind < 0) x_ind = Nx-1;
-            if (x_ind  >= Nx) x_ind = 0;
-            if (y_ind < 0) y_ind = Ny-1;
-            if (y_ind >= Ny) y_ind = 0;
-
-
-            // record position of empty cell
-            if (fish_population[x_ind][y_ind] == 0 && shark_population[x_ind][y_ind] == 0){
-              ix_free[counter_free] = x_ind;
-              jy_free[counter_free] = y_ind;
-              counter_free++;
-            }
-
-
-
-          } // end if i!=0 && j!=0
-
-
-        }  // end for j
-      } // end for i
-
-
-
-
-
-
-      // if the fish finds an empty neigboring cell
-      if (counter_free != 0){
-        // std:: cout << "empty cell movement" << std:: endl;
-        int temp1 = rand() % counter_free;
-
-        unsigned int iix = ix_free[temp1];
-        unsigned int jjy = jy_free[temp1];
-
-
-        fish_population[iix][jjy] = new_pos_age;  // move shark to new position with updated age
-        fish_population[ix][jy] = old_pos_age;   // update age at the previous position of the shark (0 for empty cell, 1 for new shark)
-      } else {  // fish does not find an empty neighboring cell
-
-        fish_population[ix][jy] = new_pos_age;
-
-
-      }
-
 
 
 
